2b19ef7cc2c0807d12e483b9661725f30fcbc467
Raushan Sharma <raushansharma7462@gmail.com>
2024-08-28 18:02:27 +0530

    feat: :fire: background color in change password
    background color in change password

--- frontend/src/Components/Pages/Auth/ChangePassword.jsx
+++ frontend/src/Components/Pages/Auth/ChangePassword.jsx
@@ -77,7 +77,7 @@ export default function ChangePassword() {
                                     <label for="newsletter" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                                 </div>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Reset passwod</button>
+                            <button type="submit" className="w-full text-white bg-teal-600 hover:bg-teal-700 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Reset passwod</button>
                         </form>
                     </div>
                 </div>
